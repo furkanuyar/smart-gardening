b8322c136ad8dc45fc45b68c42b906ec402e9698
Furkan Uyar <furkanuyar92@gmail.com>
2016-02-24 15:45:49 +0200

    Update raspberry-part.py

--- RASPBERRY/raspberry-part.py
+++ RASPBERRY/raspberry-part.py
@@ -3,9 +3,8 @@ import serial
 
 ser = serial.Serial('/dev/ttyACM0', 9600)  // Ardunio device
 
-pubnub = Pubnub(publish_key='pub-c-77f8ee30-f9ba-4b09-815a-3ba6309ca3ed', 
-subscribe_key='sub-c-059fe690-cf78-11e5-b764-02ee2ddab7fe')
-
+pubnub = Pubnub(publish_key='pub-c-***',        # you can get your unique pubnub key
+subscribe_key='sub-c-***')                      # from pubnub official site as a free
 channel = 'check'
 
 
